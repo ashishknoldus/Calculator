33c6337f8471505760fefaa67e8b39e68c909168
Ashish <ashish.kumar@knoldus.com>
2021-08-24 15:01:54 +0530

    Updated

--- src/main/scala/com/firstApp/Calculator.scala
+++ src/main/scala/com/firstApp/Calculator.scala
@@ -14,18 +14,32 @@ class Calculation {
     a / b
   }
   //  Power
+  def power(x: Int, n: Int): Long = {
+    if (n >= 1)
+      x * power(x, n-1)
+    else 1
+  }
   //  Absolute
+  def absolute(a: Double):Double = a.abs
   //  Modulus
+  def %(a: Int, b: Int): Int = a % b
   //  Maximum of Two
+  def >(a: Int, b: Int): Int = {
+    if (a>b) a
+    else  b
+  }
   //  Minimum of Two
-
+  def <(a: Int, b: Int): Int = {
+    if (a<b)  a
+    else  b
+  }
 }
 object Calculator
 {
   implicit class PowerInt(i: Int) {
     def ** (n: Int): Int = pow(i, n).intValue
   }
-  def main(args: Array[String]) =
+  def main(args: Array[String]): Unit =
   {
     val calc = new Calculation()
 
@@ -33,9 +47,13 @@ object Calculator
     println("Subtraction: " + calc.-(10, 2))
     println("Multiplication: " + calc.*(10, 2))
     println("Division: " + calc./(10, 2))
-    println("Power: " + 10**2)
-
-
     //println("Division: " + calc./(10, 0))
+    //println("Power: " + 10**2)
+    println("Power: " + calc.power(10,2))
+    println("Absolute: " + calc.absolute(-5.6))
+    println("Modulus: " + calc.%(10,2))
+    println("Greater: " + calc.>(10,2))
+    println("Smaller: " + calc.<(10,2))
+
   }
 }
